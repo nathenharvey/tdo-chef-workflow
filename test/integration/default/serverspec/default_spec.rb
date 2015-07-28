07aebe835255ce915c23f561d7052ad8edf7ffd1
Nathen Harvey <nharvey@chef.io>
2015-07-27 22:30:44 -0400

    write our first integration tests
    they will fail

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe 'web::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'returns the home page' do
+    expect(command('curl http://localhost').exit_status).to eq 0
+  end
+
+  it 'is listening on port 80' do
+    expect(port 80).to be_listening
   end
 end
